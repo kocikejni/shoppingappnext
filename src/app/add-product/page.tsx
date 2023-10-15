e2f6ebc25c0b9c934c52b939cc80a1375efcf729
ZBook\kejni <kejnikoci2000@gmail.com>
2023-10-15 22:42:52 +0200

    Configuration&AddProductCode

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+    title: 'Add your product on Shopli'
+}
+
+async function addProduct(formData: FormData) {
+    "use server"
+    const name = formData.get('name')?.toString();
+    const description = formData.get('description')?.toString();
+    const imageURL = formData.get('imageURL')?.toString();
+    const price = Number(formData.get('price') || 0);
+
+    if (!name || !description || !imageURL || !price) {
+        throw new Error('Missing required fields');
+    }
+
+    await prisma.products.create({
+        data: { name, description, imageURL, price }
+    });
+    redirect("/")
+}
+
+export default function AddProductPage() {
+    return (
+        <div>
+            <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+            <form action={addProduct}>
+                <input required name="name" placeholder="Enter name" className="mb-3 w-full input input-bordered" />
+                <textarea required name="description" placeholder="Enter description" className="textarea-bordered textarea mb-3 w-full" />
+                <input required name="imageURL" placeholder="Upload image" className="mb-3 w-full input input-bordered" type="url" />
+                <input required name="price" placeholder="Enter price" className="mb-3 w-full input input-bordered" type="number" />
+                <button className="btn btn-primary btn-block" type="submit">Add Product</button>
+            </form>
+
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#4b6bfb",
+          secondary: "#7b92b2",
+          accent: "#67cba0",
+          neutral: "#181a2a",
+          "base-100": "#ffffff",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+          body: {
+            "background-color": "#e3e6e6",
+          },
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
